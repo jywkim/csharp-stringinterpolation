011ae18262186a84ae4a9b6991d801386abd1694
jywkim <jkim589@gmail.com>
2018-09-29 22:02:40 -0400

    specify format string for interpolated expression

--- Program.cs
+++ Program.cs
@@ -16,6 +16,12 @@ namespace csharp_stringinterpolation
           // Expected output:
           // Area of the right triangle with legs of 3 and 4 is 6
           // Length of the hypotenuse of the right triangle with legs of 3 and 4 is 5
+
+          var date = new DateTime(1731, 11, 25);
+          Console.WriteLine($"On {date:dddd, MMMM dd, yyyy} Leonhard Euler introduced the letter e to denote {Math.E:F5} in a letter to Christian Goldbach.");
+
+          // Expected output:
+          // On Sunday, November 25, 1731 Leonhard Euler introduced the letter e to denote 2.71828 in a letter to Christian Goldbach.
         }
     }
 }
